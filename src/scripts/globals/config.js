ff0614d2c7d1232e160ea0674dea60c305a5ebc1
ardyws <ardywirasaputra@gmail.com>
2022-10-22 16:30:16 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '611136675cbfe683b3137903be4c6e37',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+
+export default CONFIG;
